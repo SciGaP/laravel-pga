c8a9c950e70488dc2643dc0a76d7514a14e2dbf5
root <nipunhere@gmail.com>
2014-10-09 23:21:41 -0400

    Compute Resource still in process

--- app/views/resource/create-step3.blade.php
+++ app/views/resource/create-step3.blade.php
@@ -26,15 +26,24 @@
 
 			<div class="resource-manager-block hide">
 				<div class="select-resource-manager-type">
-				<h4>Select resource manager type</h4>
-				<select class="form-control selected-resource-manager">
-				  <option></option>
-				  <option value="local">FORK</option>
-				  <option value="ssh">PBS</option>
-				  <option value="globus">UGE</option>
-				  <option value="unicore">SLURM</option>
-				</select>
+					<h4>Select resource manager type</h4>
+					<select class="form-control selected-resource-manager">
+					  <option></option>
+					  <option value="local">FORK</option>
+					  <option value="ssh">PBS</option>
+					  <option value="globus">UGE</option>
+					  <option value="unicore">SLURM</option>
+					</select>
+				</div>
 			</div>
+
+			<div class="ssh-block hide">
+				<h5>Enter Security Protocol Details</h5>
+				<input class="form-control" name="username_password" placeholder="USERNAME_PASSWORD" maxlength="30"/>
+				<input class="form-control" name="ssh_keys" placeholder="SSH_KEYS" maxlength="30"/>
+				<input class="form-control" name="gsi" placeholder="GSI" maxlength="30"/>
+				<input class="form-control" name="kerberos" placeholder="KERBEROS" maxlength="30"/>
+				<input class="form-control" name="oauth" placeholder="OAUTH" maxlength="30"/>
 
 			</div>
 			<div class="form-group">
@@ -66,15 +75,32 @@
 			$("body").on("change", ".selected-job-protocol", function(){
 				$("div[class*='resourcemanager-']").remove();
 
-				var parentResDiv = "<div class='resourcemanager-'" + $(this).val().toLowerCase() + "><hr/>Resource Manager: <h4>" + $(this).val() + "</h4><hr/>";
+				var parentResDiv = "<div class='resourcemanager-" + $(this).val().toLowerCase() + "'>"
+										+ "<hr/>" 
+										+ "Resource Manager: <h4>" + $(this).val() + "</h4>" +
+										+ "<hr/>";
+										+ "<button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button>";
 				if( $(this).val().toLowerCase() == "local")
 				{
 					$(".job-protocol-block").append(  parentResDiv + $(".resource-manager-block").html() + "</div>" );
 				}
-				elseif( $(this).val().toLowerCase() == "ssh")
+				else if( $(this).val().toLowerCase() == "ssh")
 				{
-					$(".job-protocol-block").append(  parentResDiv + $(".ssh-block").html() + "</div>" );
+					$(".job-protocol-block").append(  parentResDiv 
+													+ $(".ssh-block").html()
+													+ $(".resource-manager-block").html() 
+													+ "<h5>Alternate SSH Host Name</h5>" 
+													+ "<input class='form-control' name='alternatesshhostname'/>"
+													+ "</div>" );
 
+				}
+				else if( $(this).val().toLowerCase() == "globus")
+				{
+					$(".job-protocol-block").append(  parentResDiv 
+													+ $(".ssh-block").html()
+													+ "<h5>Globus Gate Keeper End Point</h5>" 
+													+ "<input class='form-control' name='globusGateKeeperEndPoint'/>"
+													+ "</div>" );
 				}
 			});
 		});
