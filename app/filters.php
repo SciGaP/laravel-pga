fd92864cef0284ea3d34097037a92799c6a75024
Nipurn Doshi <nipunhere@gmail.com>
2015-03-12 11:16:57 -0400

    Multi tenancy roles and users progress

--- app/filters.php
+++ app/filters.php
@@ -105,7 +105,9 @@ Route::filter('verifyadmin', function()
 	if( Utilities::verify_login() )
 	{
 		if( !Session::has("admin"))
+		{
 			return Redirect::to("home")->with("admin-alert", true);
+		}
 	} 
 	else
 		return Redirect::to("home")->with("login-alert", true);
